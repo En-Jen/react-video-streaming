ad28ab20880697b601a17e0944629469c7678178
Jen Speak <jspeak6@gmail.com>
2021-01-11 22:42:31 -0800

    wired up google api library

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '537909141351-i11f8rsqift9f8lskguo8ocsidjl9jlk.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+
+    render() {
+        return <div>Google Auth</div>;
+    }
+}
+
+export default GoogleAuth;
